13457a4109f8aee0da1fe1ee0768ce4e533b94b3
akalosha <andrei.kalosha.leonidovich@gmail.com>
2024-04-11 07:24:36 +0300

    Remove redundant cast in issue №26

---
+++ src/main/java/my/home/_0026/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+package my.home._0026;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        int nums[] = new int[]{0,  1,  1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
