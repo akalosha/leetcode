6062deb6b9631301c36f55548b18566e79696c4f
akalosha <andrei.kalosha.leonidovich@gmail.com>
2025-11-27 08:12:13 +0300

    Solve issue №66

---
+++ src/main/java/my/home/_0066/Solution.java
@@ -0,0 +1,26 @@
+package my.home._0066;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] digits = {9};  // represents the number 123
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
